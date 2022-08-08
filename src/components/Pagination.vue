22307585e0c02df55df4c6eb2993c7decbd6f7b2
tkb0004413 <writer159.new@gmail.com>
2022-08-08 03:36:58 +0000

    feat: 返回首頁。

---
+++ src/components/Pagination.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+
+const goHome = () => {
+  if (route.name === 'Home')
+    return
+  else router.push({ name: 'Home' })
+}
+</script>
+
+<template>
+  <div class="">
+    <button @click="goHome">返回首頁</button>
+  </div>
+</template>
