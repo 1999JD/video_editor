974b96f8a9ac3341583709a5c3e7cbbd5fcabf95
tkb0004413 <writer159.new@gmail.com>
2022-08-09 03:50:46 +0000

    feat: 上船功能

---
+++ src/components/Upload.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import { useStore } from '@/stores/index'
+
+const store = useStore()
+const file = ref('file')
+
+function upload() {
+  store.videoBlob = file.value.files[0]
+}
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="upload">
+      <input ref="file" type="file">
+      <button type="submit">確認</button>
+    </form>
+  </div>
+</template>
